c1442bf271ff7f83aed6e4101895f7a9925289d6
houdinipapi <ochiengcliff.co@gmail.com>
2023-07-06 20:41:16 +0000

    Compress before sending

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -11,10 +11,10 @@ from fabric.decorators import runs_once
 def do_pack():
     """Generates .tgz archive from the contents of the web_static folder"""
     local("mkdir -p versions")
-    path = ("versions/web_static_{}.tgz"
-            .format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")))
-    result = local("tar -cvzf {} web_static"
-                   .format(path))
+    path = "versions/web_static_{}.tgz".format(
+        datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")
+    )
+    result = local("tar -cvzf {} web_static".format(path))
 
     if result.failed:
         return None
