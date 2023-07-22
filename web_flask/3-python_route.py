b47e8c337b7a096ab913f2312c08cc7ebfd7b0b0
papihoudini <ochiengcliff.co@gmail.com>
2023-07-22 10:40:08 -0400

    Python is cool

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+"""
+A script that starts a Flask web application
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_text(text):
+    """
+    Replace underscores with spaces in the text variable
+    """
+    text = text.replace("_", " ")
+    """
+    Displays 'C' followed by the value of the text variable
+    """
+    return "C {}".format(text)
+
+
+@app.route("/python/", defaults={"text": "is cool"}, strict_slashes=False)
+@app.route("/python/(<text>)", strict_slashes=False)
+def python_text(text):
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
